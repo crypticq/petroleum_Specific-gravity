5d49b260d69ebea39d8be1a61761787668861be7
crypticq <85430867+crypticq@users.noreply.github.com>
2022-03-29 03:10:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,15 +7,15 @@ def api_denisty(api):
     sg = (141.5 / api) + 131.5
     API_gravity = (141.5 / sg) - 131.5
     if API_gravity >= 35:
-        return "Specific gravity for  {} and it's Light crude Oil".format(api)
+        return "Specific gravity for  {} and it's Light crude Oil".format(API_gravity)
     if API_gravity >= 45:
-        return "Specific gravity for {} and it's Extra light oil".format(api)
+        return "Specific gravity for {} and it's Extra light oil".format(API_gravity)
     if API_gravity <= 35:
-        return "Specific gravity for {} and it's medium crude".format(api)
+        return "Specific gravity for {} and it's medium crude".format(API_gravity)
     if API_gravity <= 25:
-        return "Specific gravity for {} and it's heavy crude".format(api)
+        return "Specific gravity for {} and it's heavy crude".format(API_gravity)
     if API_gravity < 15:
-        return "Specific gravity for {} and it's very heavy oil".format(api)
+        return "Specific gravity for {} and it's very heavy oil".format(API_gravity)
 
 
 if __name__ == '__main__':
