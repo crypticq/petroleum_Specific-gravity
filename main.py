33ded4e48fd4f907a3b6b4ba8d3339a409566192
crypticq <85430867+crypticq@users.noreply.github.com>
2023-03-11 08:20:22 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,22 @@
 #!/usr/bin/env python3
 
 
-        
-def api_denisty(api):
-        
-    sg = (141.5 / api) + 131.5
-    API_gravity = (141.5 / sg) - 131.5
-    if API_gravity >= 35:
-        return "Specific gravity for  {} and it's Light crude Oil".format(API_gravity)
-    if API_gravity >= 45:
-        return "Specific gravity for {} and it's Extra light oil".format(API_gravity)
-    if API_gravity <= 35:
-        return "Specific gravity for {} and it's medium crude".format(API_gravity)
-    if API_gravity <= 25:
-        return "Specific gravity for {} and it's heavy crude".format(API_gravity)
-    if API_gravity < 15:
-        return "Specific gravity for {} and it's very heavy oil".format(API_gravity)
+def api_denisty(Specific_Gravity):
+
+    API = (141.5 / Specific_Gravity) - 131.5
+    if API >= 35:
+        return "Oil Has {} API and it's Light crude Oil".format(round(API))
+    if API >= 45:
+        return "Oil Has {} API and it's Extra light oil".format(round(API))
+    if API <= 35:
+        return "Oil has {} API and it's medium crude Oil".format(round(API))
+    if API <= 25:
+        return "Oil Has {} API and it's heavy crude Oil ".format(round(API))
+    if API < 15:
+        return "Oil has {} API and it's very heavy oil".format(round(API))
 
 
-if __name__ == '__main__':
-    print('This Script convert API for petroleum to Density .. ')
-    print('Coded By Eng Yazeed')
-    
-    api = float(input(' Your API .. : '))
+if __name__ == "__main__":
+    api = float(input(" Enter Specific Gravity:  "))
     density = api_denisty(api)
     print(density)
-    
-  
-    
-    
-    
